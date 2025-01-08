4626ce205c0dcdbcb51cbcbe19954e20dd3a69b8
HarshitSinha7 <138149358+HarshitSinha7@users.noreply.github.com>
2025-01-08 19:26:01 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc" //if we have null instead of "33ab" then the value will be 0
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//NAN means Not A Number
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+//"" => false
+//"Harshit" => true
+
+let someNumber = "55aa"
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
