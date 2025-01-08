bf7b0b658e93d1554dec92079bb8efa5a059c1bc
HarshitSinha7 <138149358+HarshitSinha7@users.noreply.github.com>
2025-01-08 15:07:28 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 112233
+let accountEmail = "harshit7@gmail.com"
+var accountPassword = "1122"
+accountCity = "Bihar"
+
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all JS code as a newer version
+
+console.log(3+3)
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
